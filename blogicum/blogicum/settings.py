4c94b442e8b1da24833df87e289daa65b64672ff
Danil Shestak <danilshestaak@gmail.com>
2026-07-09 00:02:47 +0500

    Add email server

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -34,6 +34,12 @@ CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
 
 MEDIA_ROOT = BASE_DIR / 'media' 
 
+
+# Подключаем бэкенд filebased.EmailBackend:
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# Указываем директорию, в которую будут сохраняться файлы писем:
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails' 
+
 # Application definition
 
 INSTALLED_APPS = [
